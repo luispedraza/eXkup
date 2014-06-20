99d4bdfff57e779df515073fb2ddc3dea67fa2ef
Luis Pedraza <luispedraza@gmail.com>
2014-06-20 10:41:24 +0200

    comenzando el archivo de compilación

--- build.py
+++ build.py
@@ -2,8 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import os 
+import shutil
 
-for path, directories, files in os.walk('./src'):
+SOURCE_DIR = 'src'
+BUILD_DIR = 'build'
+
+SOURCE_PATH = os.path.join('.', SOURCE_DIR)
+BUILD_PATH = os.path.join('.', BUILD_DIR)
+
+try:
+	shutil.rmtree(BUILD_PATH)
+except OSError as e:
+	pass
+
+os.mkdir(BUILD_PATH)		# el directorio de destino
+
+for path, directories, files in os.walk(SOURCE_PATH):
     print 'ls %r' % path
     for directory in directories:
         print '    d%r' % directory
