8536d5dfe16ebd5073a6e4175aed119df74a56e2
Luis Pedraza <luispedraza@gmail.com>
2014-06-20 13:21:56 +0200

    algunos avances

--- build.py
+++ build.py
@@ -3,12 +3,21 @@
 
 import os 
 import shutil
+from bs4 import BeautifulSoup as BS
+import subprocess
 
 SOURCE_DIR = 'src'
 BUILD_DIR = 'build'
+TOOLS_DIR = 'build-tools'
+
+JS_COMPRESSOR = 'yuicompressor-2.4.8.jar'
+CSS_COMPRESSOR = 'yuicompressor-2.4.8.jar'
 
 SOURCE_PATH = os.path.join('.', SOURCE_DIR)
 BUILD_PATH = os.path.join('.', BUILD_DIR)
+JS_TOOL = os.path.join('.',TOOLS_DIR, JS_COMPRESSOR)
+CSS_TOOL = os.path.join('.',TOOLS_DIR, CSS_COMPRESSOR)
+
 
 try:
 	shutil.rmtree(BUILD_PATH)
@@ -18,8 +27,44 @@ except OSError as e:
 os.mkdir(BUILD_PATH)		# el directorio de destino
 
 for path, directories, files in os.walk(SOURCE_PATH):
-    print 'ls %r' % path
-    for directory in directories:
-        print '    d%r' % directory
-    for filename in files:
-        print '    -%r' % filename
+	print path
+	relpath = os.path.relpath(path, SOURCE_PATH)
+	print relpath
+	dest_path = os.path.join(BUILD_PATH, relpath)
+	# creación del directorio de destino
+	if not os.path.exists(dest_path):
+		os.makedirs(dest_path)
+	# manipulación de los a     rchivos
+	for filename in files:
+		print "archivo: " + filename
+		file_path = os.path.join(path, filename)	# ruta del archivo
+		ext = os.path.splitext(filename)			# extensión del archivo
+		# archivos html
+		if ext[1] == '.html':
+			thefile = open(file_path)
+			parsed_html = BS(thefile)
+			# procesamiento del html
+			scripts = parsed_html.findAll("script")
+			for s in scripts:
+				print s
+				print s.get('src')
+
+			
+
+			# cerramos el archivo
+			thefile.close()
+
+		#archivos js
+		elif ext[1] == '.js':
+			pass
+		# resto de archivos
+		else:
+			print filename
+			print "copiando " + file_path + " a " + dest_path
+			shutil.copy2(file_path, dest_path)
+
+
+
+
+			
+
