fbc4a78c38ee2ff6a6b0805472bd8767a6d78db3
Luis Pedraza <luispedraza@gmail.com>
2014-06-21 08:43:05 +0200

    agregados archivos faltantes

---
+++ src/exe/getimages.js
@@ -0,0 +1,5 @@
+﻿var images = [].slice.apply(document.getElementsByTagName('img'));
+images = images.map(function(element) {
+	return element.src;
+});
+result = {"title": document.title, "images": images};
