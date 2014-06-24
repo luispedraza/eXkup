a2cef53ceb12548327559b10da20d4ac6aabab48
Luis Pedraza <luispedraza@gmail.com>
2014-06-24 17:50:36 +0200

    intento de filtrado

--- src/js/d3Thread.js
+++ src/js/d3Thread.js
@@ -1,4 +1,5 @@
 var PREPOSICIONES = {a:true,ante:true,bajo:true,cabe:true,con:true,contra:true,de:true,desde:true,durante:true,en:true,entre:true,hacia:true,hasta:true,mediante:true,para:true,por:true,según:true,sin:true,so:true,sobre:true,tras:true,versus:true,vía:true};
+var VISUALIZER = null;
 
 function updateButtons() {
 	// Respuestas a mensajes:
@@ -25,7 +26,8 @@ function populateMessages(tree) {
 			$this.removeClass('on current').css("width", width).siblings().css( "width", width);
 		});
 	};
-	function accordion($element, event) {
+	function accordion(event) {
+		var $element = $(this);
 		event.stopPropagation();
 		var $children = $element.children();
 		var nChildren = $children.length;
@@ -60,13 +62,8 @@ function populateMessages(tree) {
 		});
 		return false;
 	};
-	function accordionReset($element) {
-		var $children = $element.children();
-		if($children.filter(".on").length) return;	// hay un mensaje hijo mostrado
-		var width = (100/$children.length) +  "%";
-		$children.each(function() {
-			$(this).css("width", width);
-		});
+	function accordionReset() {
+		$(this).find(".msg-container:not(.on)").css("width","");
 	};
 	function appendMessage(msg, $container) {
 		var $msg = createMessage(msg);
@@ -86,29 +83,27 @@ function populateMessages(tree) {
 		if (msg.children) {
 			var $childrenContainer = $("<div>").addClass('children-container').appendTo($msgContainer);
 			$childrenContainer
-			.on("mousemove", function(e) {
-				accordion($(this), e);
-			})
-			.on("mouseout", function(e) {
-				// accordionReset($(this));
-			});
+			.on("mousemove", accordion)
+			.on("mouseleave", accordionReset);
 			var width = (100/msg.children.length) +  "%";
 			msg.children.forEach(function(m) {
-				appendMessage(m, $childrenContainer).css("width", width);
+				appendMessage(m, $childrenContainer);//.css("width", width);
 			});
 		};
 		return $msgContainer;
 	};
-	$mainContainer = $("#chart-ouput"); // .on("mouseout", function());
+	$mainContainer = $("#chart-ouput");
 	$rootContainer = $("<div>").addClass('children-container').appendTo($mainContainer);
 	appendMessage(tree, $rootContainer).addClass('on').off();		// conversación de mensajes
 };
 
-
-
-function DataProcessor(data) {
+/* Función principal de procesamiento de datos
+	@param data: datos obtenidos de la api para una conversación
+	@ param hidden: inicialmente todas las respuestas están ocultas
+*/
+function DataProcessor(data, hide) {
+	if (typeof hide == "undefined") hide = false;
 	var THAT = this;
-	var infoTree;
 	this.words = {};
 	this.images = {};
 	this.videos = {};
@@ -117,15 +112,21 @@ function DataProcessor(data) {
 	/* Convierte la estructura de partida en un árbol para d3js */
 	function makeInfoTree(info) {
 		var messages = info.mensajes;
-		var index = {};
+		var index = {};		// variable auxiliar para encontrar rápido los mensajes
 		var rootMsg = messages[0];
 		index[rootMsg.idMsg] = rootMsg;
 		var replies = messages.slice(1);
 		replies.forEach(function(m) {
 			var parentID = m.idMsgRespuesta;
 			var parentObj = index[parentID];
-			var children = (parentObj.children || (parentObj.children = []));
-			children.push(m);
+			m.parent = parentObj;	// el padre de este mensaje
+			if (hide) {
+				var hidden = (parentObj._hidden || (parentObj._hidden = []));
+				hidden.push(m);
+			} else {
+				var children = (parentObj.children || (parentObj.children = []));
+				children.push(m);
+			};
 			index[m.idMsg] = m;
 			messageProcessor(m);
 		});
@@ -142,11 +143,118 @@ function DataProcessor(data) {
 		userMsgs.push(id);
 		// imagen
 		if (m.cont_adicional) {
-			var item = images[m.cont_adicional] || (images[m.cont_adicional] = {ids: []});
-			item.ids.push(id);
+			var imgMsgs = images[m.cont_adicional] || (images[m.cont_adicional] = {messages: []});
+			imgMsgs.messages.push(id);
 		};
 	};
+	/* Filtrado de resultados en el árbol 
+		@param filter. de la forma {user: username, word: word, photo: photo_url, video, video_url}
+	*/
+	this.filterAdd = function(filter, node) {
+		function addFound(msg) {
+			var parent = msg.parent;
+			if (!parent) return;
+			var pChildren = (parent.children || (parent.children = []));
+			if (pChildren.indexOf(msg)>=0) return;	// ya está agregado
+			// ¿Es un nodo filtrado?
+			var pHidden = parent._hidden;
+			if (pHidden) {
+				var index = pHidden.indexOf(msg);
+				if (index>=0) {
+					parent.children = pChildren.concat(pHidden.splice(index, 1));
+					addFound(parent);
+					return;
+				};
+			};
+			// ¿Es un nodo colapsado?
+			var pCollapsed = parent._children;
+			if (pCollapsed) {
+				var index = pCollapsed.indexOf(msg);
+				if (index>=0) {
+					parent.children = pChildren.concat(pCollapsed.splice(index, 1));
+					addFound(parent);
+					return;
+				};
+			};
+		};
+		if (typeof node === "undefined") {
+			node = THAT.tree;	// el nodo raíz, que nunca se oculta o filtra
+		} else if (filter.user) {
+			if (node.usuarioOrigen == filter.user) {
+				addFound(node);
+			};
+		};
+		if (node._hidden) {
+			// recursión sobre los hijos filtrados
+			var _hidden = node._hidden;
+			for (var i=_hidden.length-1; i>=0; i--) {
+				THAT.filterAdd(filter, _hidden[i]);
+			};
+		};
+		if (node._children) {
+			// recursión sobre los hijos ocultos
+			var _children = node._children;
+			for (var i=_children.length-1; i>=0; i--) {
+				THAT.filterAdd(filter, _children[i]);
+			};
+		};
+	};
+	/* Eliminación de un filter */
+	this.filterRemove = function(filter, node) {
+		function removeFound(msg) {
+			var parent = msg.parent;
+			if (!parent) return;
+			var pHidden = (parent._hidden || (parent._hidden = []));
+			if (pHidden.indexOf(msg)>=0) return;	// ya está oculto
+			// ¿Es un nodo visible?
+			var pChildren = parent.children;
+			if (pChildren) {
+				var index = pChildren.indexOf(msg);
+				if (index>=0) {
+					parent._hidden = pHidden.concat(pChildren.splice(index, 1));
+					removeFound(parent);
+					return;
+				};
+			};
+			// ¿Es un nodo colapsado?
+			var pCollapsed = parent._children;
+			if (pCollapsed) {
+				var index = pCollapsed.indexOf(msg);
+				if (index>=0) {
+					parent._hidden = pHidden.concat(pCollapsed.splice(index, 1));
+					removeFound(parent);
+					return;
+				};
+			};
+		};
+		if (typeof node === "undefined") {
+			node = THAT.tree;	// el nodo raíz, que nunca se oculta o filtra
+		} else if (filter.user) {
+			if (node.usuarioOrigen == filter.user) {
+				removeFound(node);
+			};
+		};
+		if (node.children) {
+			// recursión sobre los hijos no filtrados
+			var children = node.children;
+			for (var i=children.length-1; i>=0; i--) {
+				THAT.filterRemove(filter, children[i]);
+			};
+		};
+		if (node._children) {
+			// recursión sobre los hijos ocultos
+			var _children = node._children;
+			for (var i=_children.length-1; i>=0; i--) {
+				THAT.filterAdd(filter, _children[i]);
+			};
+		};
+	};
+	// Procesamiento de los datos:
+	this.tree = makeInfoTree(data);
+};
 
+function ConversationController(processor) {
+	var PROC = processor;
 	function insertMessage(id, clean) {
 		var $ouput = $('#chart-ouput');
 		if (typeof clean == "undefined") clean=true;
@@ -156,24 +264,36 @@ function DataProcessor(data) {
 			$ouput.append(createMessage(THAT.messages[id]));	
 		};
 	};
-
-	this.populateUsers = function() {
-		var theUsers = makeArray(THAT.users);
+	function populateUsers() {
+		var theUsers = makeArray(PROC.users);
 		var li = d3.select("#users-list").selectAll("li").data(theUsers);
 		li.enter().append("li")
 			.text(function(d){return "@"+d.__key})
+			.attr("class", "check")
 			.on("mouseover", function(d) {
 				d3.selectAll("g.node")
 					.attr("opacity", function(n) {
 						return (n.usuarioOrigen == d.__key) ? 1 : .1;
 					});
 			})
+			.on("mouseout", function(d) {
+				d3.selectAll("g.node")
+					.attr("opacity", 1);
+			})
 			.on("click", function(d) {
-				insertMessage(THAT.users[d.__key].messages);
+				$this = $(this);
+				$this.toggleClass('on');
+				var e = document.createEvent("CustomEvent");
+				var detail = {'user': d.__key,
+					'type': $this.hasClass('on') ? "add" : "remove"
+					};
+				e.initCustomEvent("updateFilter", false, false, detail);
+				document.getElementById("svg").dispatchEvent(e);
+				// insertMessage(THAT.users[d.__key].messages);
 			});
 	};
-	this.populateImages = function() {
-		var theImages = makeArray(THAT.images);
+	function populateImages() {
+		var theImages = makeArray(PROC.images);
 		var li = d3.select("#images-list").selectAll("li").data(theImages);
 		li.enter().append("img")
 			.attr("src", function(d){return d.__key})
@@ -184,45 +304,24 @@ function DataProcessor(data) {
 					});
 			})
 			.on("click", function(d) {
-				d.ids.forEach(function(id) {
-					insertMessage(id);
-					updateButtons();
+				d.messages.forEach(function(id) {
+					// insertMessage(id);
+					// updateButtons();
 				});
 			});
 	};
-	// PRocesamiento de los datos:
-	this.tree = makeInfoTree(data);
-};
+	function populateVideos(videos) {
 
+	};
+	function populateWords(words) {
 
-function populateVideos(videos) {
-
-};
-function populateWords(words) {
-
-};
-
-function d3Translate(vector) {
-	return " translate(" + vector + ")";
-};
-function d3Scale(scale) {
-	return " scale(" + scale + ")";
-};
-function d3Rotate(rot) {
-	return " rotate(" + rot + ")";
-};
-function d3Matrix(matrix) {
-	return " matrix("+matrix.join(",")+")"
-};
-function d3PolyPath(r, n) {
-	var points = [],
-        alfa = Math.PI/n,	// initial angle
-        delta = alfa*2;
-    do {
-    	points.push([r*Math.cos(alfa), r*Math.sin(alfa)].join(","));
-    	alfa += delta;
-    } while (alfa < 2*Math.PI);
-    return points.join(" ");
+	};
+	this.populate = function() {
+		populateUsers();
+		populateImages();
+		populateVideos();
+		populateWords();
+	};
 };
 
 function expandToElement(root, callbackCondition) {
@@ -233,7 +332,6 @@ function expandToElement(root, callbackCondition) {
 	};
 };
 
-
 // visualización en árbol:
 function TalkVisualizer(data) {
 	var RADIUS_DEFAULT = 4,
@@ -241,12 +339,11 @@ function TalkVisualizer(data) {
 	// Manipulación de los datos
 	// console.log(data);
 	// console.log(JSON.stringify(data));
-	var PROCESSOR = new DataProcessor(data);
+	var PROCESSOR = new DataProcessor(data, true);
+	var CONTROLLER = new ConversationController(PROCESSOR);
 
 	var root = PROCESSOR.tree;
-	
-	
-	// console.log(root);
+	console.log(root);
 
 	var center = {x: window.innerWidth/2, y: window.innerHeight/2};
 
@@ -255,7 +352,7 @@ function TalkVisualizer(data) {
 
 	// Funciones de dibujo:
 	var diameter = 960;
-	var tree = d3.layout.tree()
+	var treeLayout = d3.layout.tree()
 		.size([360, diameter / 2 - 120])
 		.separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
@@ -267,37 +364,49 @@ function TalkVisualizer(data) {
 		.scaleExtent([.25,8])
 		.on("zoom", function() {
 			var vector = d3.event.translate;
-			//vector = [center.x+vector[0], center.y+vector[1]];
+			// vector = [center.x+vector[0], center.y+vector[1]];
 			chart.attr("transform", d3Translate(vector) + d3Scale(d3.event.scale));
 			RADIUS = RADIUS_DEFAULT / d3.event.scale;
 		})
 		.on("zoomend", function() {
-			svg.selectAll(".node circle")
+			chart.selectAll(".node circle")
 				.transition().duration(duration)
 				.attr("r", RADIUS);	// escalado de los nodos
 		});
 	var svg = d3.select("body").insert("svg", "#chart-control")
+		.attr("id", "svg")
 		.on("dblclick", function() {
-			zm.translate([diameter/2,diameter/2]);
+			zm.translate([0,0]);
 			zm.scale(1);
 			zm.event(svg);
 		})
 		.call(zm)
 		.on("dblclick.zoom", null);
-	var chart = svg.append("g");
-		// .attr("transform", d3Translate([center.x,center.y]))
-		// .append("g");
+
+	var chart = svg.append("g")
+				.attr("transform", d3Translate([center.x,center.y]))
+				.append("g").attr("id", "chart");
+
+	/* Actualización de filtros */
+	document.getElementById("svg").addEventListener("updateFilter", function(e) {
+		var type = e.detail.type;
+		if (type==="add") {
+			PROCESSOR.filterAdd(e.detail);
+			update();
+		} else if (type==="remove") {
+			PROCESSOR.filterRemove(e.detail);
+			update();
+		};
+	});
 	
 	//root.children.forEach(collapse);
 	// collapse(root);
 
-	PROCESSOR.populateUsers();
-	PROCESSOR.populateImages();
+	CONTROLLER.populate();
 	populateMessages(root);
 	var nodes, links;
 	var $currentMessage = null;
-	//root.children.forEach(collapse);
-	update(root);
+	update();
 
 	function stashPositions() {
 		nodes.forEach(function(d) {
@@ -316,8 +425,9 @@ function TalkVisualizer(data) {
 		};
 		update(d);
 	};
+	/* Colapsa los nodos de la estructura de datos de árbol */
 	function collapse(d) {
-		if (d.children) {
+		if (d.children.length) {
 	  		d._children = d.children;
 	  		d._children.forEach(collapse);
 	  		d.children = null;
@@ -328,6 +438,7 @@ function TalkVisualizer(data) {
 
 	/* Principal función de actualización */
 	function update(source) {
+		if (typeof source == "undefined") source = root;
 		// Projected position of source node:
 		function updateNodes() {
 			var node = chart.selectAll(".node")
@@ -416,18 +527,19 @@ function TalkVisualizer(data) {
 			updateNodes();
 			updateTreeLinks();
 		};
-		nodes = tree.nodes(root); //.reverse()
-		links = tree.links(nodes);
+		nodes = treeLayout.nodes(root); //.reverse()
+		links = treeLayout.links(nodes);
 		updateSVG();
 		// Stash the old positions for transition.
 		stashPositions();
+		
 	};
 };
 
 
-var VISUALIZER = null;
+
 /* se obtiene la información de la extensión */
-var TEST = 2;
+var TEST = 0;
 
 if ((typeof SAMPLE_DATA != "undefined") && (typeof TEST != "undefined")) {
 	if (TEST === 0) {
@@ -445,4 +557,12 @@ if ((typeof SAMPLE_DATA != "undefined") && (typeof TEST != "undefined")) {
 			VISUALIZER = new TalkVisualizer(request.info);
 		});	
 };
+
+/* EVENTOS */
+// selección de todos los usuarios
+$("#check-all-users").on("click", function() {
+	var $this = $(this);
+	$this.toggleClass('on');
+	$("#users-list li").toggleClass('on', $this.hasClass('on'));
+});
 
