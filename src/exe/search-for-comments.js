865977d109546d8a900ec75385faf8535d942f9a
Luis Pedraza <luispedraza@gmail.com>
2015-05-11 17:47:25 +0200

    agregado soporte para las webs de cincodias, los40, cadenaser, y as, además de elpais.com y eskup

--- src/exe/search-for-comments.js
+++ src/exe/search-for-comments.js
@@ -1,40 +1,39 @@
 (function() {
-	var currentLocation = window.location.href;
-	var elpaisPattern = RegExp("https?://(.*?)\.?elpais\.com");
-	var found = elpaisPattern.exec(currentLocation);
+	var host = window.location.host;
 	var result = null;
-	console.log(found);
-	if (found) {
-		if (found[1] == "eskup") {
-			// es una página de eskup
-			found = currentLocation.match(/eskup\.elpais\.com\/C([\w-]+)\/?.*$/);	// conversación
-			if (found) {
-				result = {type: "thread", id: found[1]};
-			} else {
-				found = currentLocation.match(/eskup\.elpais\.com\/\*([\w-]+)$/);	// tema
-				if (found) {
-					result = {type: "theme", id: found[1]};		
-				} else {
-					found = currentLocation.match(/eskup\.elpais\.com\/([\w]+)\/?$/);		// usuario
-					if (found && (found.length<40)) {
-						result = {type: "user", id: found[1]};
-					}
-				};
-			};
+	if (host === "eskup.elpais.com") {
+		// es una página de eskup
+		var currentLocation = window.location.href;
+		var found = currentLocation.match(/eskup\.elpais\.com\/C([\w-]+)\/?.*$/);	// conversación
+		if (found) {
+			result = {type: "thread", id: found[1]};
 		} else {
-			// es una noticia de elpais.com
-			var commentsWidget = document.querySelector(".bloque_comentarios iframe");
-			if (commentsWidget) {
-				var commentsWidgetID = commentsWidget.id;
-				var threadID = commentsWidgetID.match("comentarios_noticia_(.+)")[1];
-				if (threadID) {
-					return {
-						type: "thread",
-						id: threadID
-					};
-				};
+			found = currentLocation.match(/eskup\.elpais\.com\/\*([\w-]+)$/);	// tema
+			if (found) {
+				result = {type: "theme", id: found[1]};		
+			} else {
+				found = currentLocation.match(/eskup\.elpais\.com\/([\w]+)\/?$/);		// usuario
+				if (found && (found.length<40)) {
+					result = {type: "user", id: found[1]};
+				}
 			};
 		};
+	} else if (host.match(/(.+\.)?(elpais|cincodias|as|cadenaser|los40)\.com/)) {
+		// es una noticia en un medio
+		var iframes = document.querySelectorAll("iframe");
+		for (var i=0; i<iframes.length; i++) {
+			var iframeID = iframes[i].id;
+			if (iframeID) {
+				var match = iframeID.match("comentarios_noticia_(.+)");
+				if (match) {
+					result =  {
+						type: "thread",
+						id: match[1]
+					};
+					break;
+				};
+			}
+		}
 	};
 	return result;
 })();
